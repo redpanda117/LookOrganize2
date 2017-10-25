5db66a5ae6dc95a13bb9e31394b55e1381d7027b
Tina Nguyen <nguyentina2005@yahoo.com>
2017-10-25 10:57:18 -0400

    calendar

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Notes from "./pages/Notes";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
-import Calendar from "./pages/Calendar";
+import MySchedule from "./pages/Calendar";
 import Register from "./pages/Register";
 
 const App = () =>
@@ -16,7 +16,7 @@ const App = () =>
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/logout" component={Login} />
-        <Route exact path= "/schedule/" component={Calendar}/>
+        <Route exact path= "/schedule/" component={MySchedule}/>
         <Route exact path="/notes/" component={Notes} />
         <Route exact path="/notes/:id" component={Detail} />
         <Route component={NoMatch} />
