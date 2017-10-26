3ab957a3e40d8f986830480ec78e3609cf38a3cf
Tina Nguyen <nguyentina2005@yahoo.com>
2017-10-26 10:57:18 -0400

    jumbotron img

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,7 +1,16 @@
 import React from "react";
+import backgroundPic from"../../img/organize.png";
 
+const JumboStyle = {
+  backgroundImage: `url(${backgroundPic})`,
+  backgroundSize: "cover",
+  height: "500px",
+  color: "#FFFFFF",
+  textAlign: 'center',
+  textDecoration: "underline"
+}
 const Jumbotron = ({ children }) =>
-  <div style={{ height: 300 }} className="jumbotron">
+  <div style={ JumboStyle } className="jumbotron">
     {children}
   </div>;
 
